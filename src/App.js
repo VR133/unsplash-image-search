7f5e0a92dbe29a4b1422e353e121f63331a63384
Victor Reaboi <victor.reaboi@mediapark.com>
2022-03-12 21:19:18 +0200

    project setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import {ThemeProvider} from 'styled-components'
+import {defaultTheme} from './shared/styles/Themes.styles'
+import GlobalStyle from './shared/styles/Global.styles'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return <ThemeProvider theme={defaultTheme}>
+        <GlobalStyle />
+        <BrowserRouter>
+            <main className='py-3'>
+                <div>Header</div>
+                <Routes>
+                    <Route path='/' element={<div>salut</div>}/>
+                </Routes>
+                <div>Footer</div>
+            </main>
+        </BrowserRouter>
+    </ThemeProvider>
 }
 
 export default App;
---
+++ src/shared/styles/Global.styles.js
@@ -0,0 +1,15 @@
+import {createGlobalStyle} from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+body {
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    sans-serif;
+}
+
+* {
+  margin: 0;
+}
+`
+
+export default GlobalStyle
