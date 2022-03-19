ab9efa744e7a6ce1bf8b2d3bc8ab1753536ff280
Victor Reaboi <victor.reaboi@mediapark.com>
2022-03-19 13:06:13 +0200

    image block to one component

--- src/components/ImageBlock/ImageBlock.jsx
+++ src/components/ImageBlock/ImageBlock.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
-import UserInfo from './UserInfo'
-import {StyledImage, StyledImageBlock, StyledTags} from './_.styles'
+import {StyledImage, StyledImageBlock, StyledTags, StyledUserInfo} from './ImageBlock.styles'
 
 const ImageBlock = ({item}) => {
 
     return <StyledImageBlock>
-        <UserInfo user={item.user}/>
+        <StyledUserInfo>
+            <img src={item.user.profile_image.small} alt=""/>
+            <div>{item.user.first_name} {item.user.last_name}</div>
+        </StyledUserInfo>
         <StyledImage src={item.urls.regular} alt={item.alt_description} />
         <StyledTags>
             {item.tags.map((tag, index) => <span key={index + tag.title}>#{tag.title}&nbsp;</span>)}
