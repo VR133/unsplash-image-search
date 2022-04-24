ba07206731ffbee398d87f8198e49c807d8412f8
Victor Reaboi <victor.reaboi@mediapark.com>
2022-04-24 21:09:13 +0300

    DropDown.jsx readability

--- src/components/DropDown/DropDown.jsx
+++ src/components/DropDown/DropDown.jsx
@@ -5,10 +5,17 @@ const DropDown = ({triggerTittle, children}) => {
 
     const [showDropDown, setShowDropDown] = useState(false)
 
+    const triggerClickHandler = () => {
+        setShowDropDown((currState) => !currState)
+    }
+
     return <StyledDropDown>
-        <Trigger onClick={() => setShowDropDown((currState) => !currState)}>{triggerTittle}</Trigger>
-        {showDropDown ? <DropDownItems>{children}</DropDownItems> : null}
-    </StyledDropDown>;
+        <Trigger onClick={triggerClickHandler}>{triggerTittle}</Trigger>
+        {showDropDown
+            ? <DropDownItems>{children}</DropDownItems>
+            : null
+        }
+    </StyledDropDown>
 };
 
 export default DropDown
